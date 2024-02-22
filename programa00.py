f88672f27c64d34ec461e287cd03fa580d47cdcf
leydigaba <160621392+leydigaba@users.noreply.github.com>
2024-02-21 21:11:08 -0600

    Create programa00.py

---
+++ programa00.py
@@ -0,0 +1,8 @@
+"""
+   Nombre: Leydi Jaret Gallegos Badillo
+   Fecha: 18/01/2024
+   Descripcion: Imprime el mensaje de "hola mundo"
+
+"""
+
+print("hola mundo python!!") #Imprime el texto "Hola mundo python!!"
