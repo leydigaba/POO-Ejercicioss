1611ca617defb385dff20db7570cd8a673a45a54
leydigaba <160621392+leydigaba@users.noreply.github.com>
2024-02-21 23:36:04 -0600

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+Type "help", "copyright", "credits" or "license" for more information.
+>>> first_number = int(input('Type the first number: ')) ;\
+... second_number = int(input('Type the second number: ')) ;\
+... print("The sum is: ", first_number + second_number)
+Type the first number: 2
+Type the second number: 3
+The sum is:  5
