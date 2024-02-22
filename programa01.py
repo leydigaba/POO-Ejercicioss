fa134419a85e7383187ee440b4d95b4e7242da4b
leydigaba <160621392+leydigaba@users.noreply.github.com>
2024-02-21 21:12:26 -0600

    Create programa01.py

---
+++ programa01.py
@@ -0,0 +1,13 @@
+"""
+   Nombre: Leydi Jaret Gallegos Badillo
+   Fecha: 18/01/2024
+   Descripcion: Programa que crea un objeto
+"""
+
+class A:  #Define una clase de nombre A
+    matricula = None  # Define el nombre del valor matricula
+    nombre = None  # Declara la variable del nombre
+
+
+objetoA = A()  #Genera una instancia de la clase A
+print(objetoA.nombre)  # Imprime el valor del nombre
